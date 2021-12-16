cf4ea420b502ee3c09df5212ec1bff84912c0ae3
rodzilla <rrrodzilla@protonmail.com>
2021-12-15 22:31:50 -0800

    Swap out logging for instrumentation via tracing

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,63 @@
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpResponse, HttpServer};
-use env_logger::Env;
+use std::net::TcpListener;
+
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 use thiserror::Error;
+use tracing::subscriber::{set_global_default, SetGlobalDefaultError};
+use tracing_actix_web::TracingLogger;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::{log_tracer::SetLoggerError, LogTracer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[derive(Debug, Error)]
 pub enum LetMeInServerError {
     #[error(transparent)]
     InitiateServerError(#[from] std::io::Error),
+    #[error(transparent)]
+    FromLoggerError(#[from] SetGlobalDefaultError),
+    #[error(transparent)]
+    SetLoggerError(#[from] SetLoggerError),
 }
 
-fn config_app(cfg: &mut web::ServiceConfig) {
+#[tracing::instrument(skip(cfg))]
+fn config_web_app(cfg: &mut web::ServiceConfig) {
+    //build all the routes
     cfg.service(web::resource("/pulse").route(web::get().to(HttpResponse::Ok)));
 }
 
-#[actix_web::main]
-pub async fn init_service() -> Result<Server, LetMeInServerError> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
-    let server = HttpServer::new(|| App::new().wrap(Logger::default()).configure(config_app))
-        .bind("127.0.0.1:8000")?
-        .run();
+//#[actix_web::main]
+pub async fn init_service(tcp_listener: TcpListener) -> Result<Server, LetMeInServerError> {
+    //initialize logging
+    LogTracer::init()?;
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("letmein".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber)?;
+    //  let span = tracing::debug_span!("init_service");
+    //  let _enter = span.enter();
+
+    //set up and return server
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(TracingLogger::default())
+            .configure(config_web_app)
+    })
+    .listen(tcp_listener)?
+    .run();
+
     Ok(server)
 }
 
 #[cfg(test)]
 mod unit_tests {
-    use super::config_app;
+    use super::config_web_app;
     use actix_web::{test, App};
 
     #[actix_rt::test]
     async fn test_heartbeat_response() {
-        let mut app = test::init_service(App::new().configure(config_app)).await;
+        let mut app = test::init_service(App::new().configure(config_web_app)).await;
         let req = test::TestRequest::get().uri("/pulse").to_request();
         let resp = test::call_service(&mut app, req).await;
 
@@ -37,7 +66,7 @@ mod unit_tests {
 
     #[actix_rt::test]
     async fn test_heartbeat_response_length() {
-        let mut app = test::init_service(App::new().configure(config_app)).await;
+        let mut app = test::init_service(App::new().configure(config_web_app)).await;
         let req = test::TestRequest::get().uri("/pulse").to_request();
         let bytes = test::read_response(&mut app, req).await;
 
--- tests/smoke_tests.rs
+++ tests/smoke_tests.rs
@@ -1,12 +1,29 @@
+use std::net::TcpListener;
+
 #[actix_rt::test]
 async fn health_check_test() -> anyhow::Result<()> {
-    letmein::init_service()?.await?;
-
+    let service_address = spawn_server().await?;
     let client = reqwest::Client::new();
 
-    let response = client.get("http://127.0.0.1:8000/pulse").send().await?;
+    let response = client
+        .get(&format!("{}/pulse", service_address))
+        .send()
+        .await?;
 
     assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 
     Ok(())
+}
+
+async fn spawn_server() -> anyhow::Result<String> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+
+    let service_address = format!("http://127.0.0.1:{}", listener.local_addr()?.port());
+
+    let server = letmein::init_service(listener);
+    //let _server = server.await?;
+    let _ = tokio::spawn(async { server.await.unwrap().await });
+
+    Ok(service_address)
 }
